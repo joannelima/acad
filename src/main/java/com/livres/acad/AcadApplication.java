6717dceb52f1cc7cced7a912a81f9a2c91bb93f0
Joanne Lima <joanne.lima@ifood.com.br>
2023-11-18 01:51:45 -0300

    first commit

---
+++ src/main/java/com/livres/acad/AcadApplication.java
@@ -0,0 +1,13 @@
+package com.livres.acad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcadApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcadApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/livres/acad/AcadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.livres.acad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
